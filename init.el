2c8a0b598e05e148fd8ea0401ff93a017b0088a8
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-08-29 18:51:07 +1000

    Added ido-mode and enabled flex matching

--- init.el
+++ init.el
@@ -13,15 +13,24 @@
 ;; uses "default" keybindings for multiple-cursors
 
 (custom-set-variables
- '(backup-directory-alist (quote (("." . "backups")))) ;; put the backups away nicely
- '(custom-enabled-themes (quote (wombat))) ;; wombat is pretty good
- '(indent-tabs-mode nil) ;; I like spaces
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote (("." . "backups"))))
+ '(custom-enabled-themes (quote (wombat)))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(lisp-body-indent 4)
  '(sgml-basic-offset 4)
  '(tool-bar-mode nil))
 (custom-set-faces
-
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 ;; MELPA
